fb3879635096893bebc4e960488ad99c5ce183fd
adamstaylora <31190298+adamstaylora@users.noreply.github.com>
2018-10-31 10:49:55 -0500

    Update bolton.py
    add comments to definitions

--- skewT/bolton.py
+++ skewT/bolton.py
@@ -11,36 +11,46 @@ k_dry = 0.2854
 
 
 def sat_vapor_pressure(T):
-    e_s = 6.112*np.exp((17.67*T)/(T+243.5)) #celsius input, hPa output
+    '''Takes a temperature (C) and returns saturation vapor pressure (hPa) via eqn 10 (Bolton, 1980)'''
+    e_s = 6.112*np.exp((17.67*T)/(T+243.5))
     return e_s
 def sat_vapor_temperature(e_s):
-    T = (243.5*np.log(e_s)-440.8)/(19.48-np.log(e_s)) #hPa input, celsius output
+    '''Takes a saturation vapor pressure (hPa) and returns a temperature (C) via eqn 11 (Bolton, 1980)'''
+    T = (243.5*np.log(e_s)-440.8)/(19.48-np.log(e_s))
     return T
 def sat_mixing_ratio(P, T):
-    w = eps*sat_vapor_pressure(T)/(P-sat_vapor_pressure(T)) #hPa and celsius input, hPa from sat_vapor_pressure, kg/kg output
+    '''Takes a pressure (hPa) and temperature (C) and returns a mixing ratio (kg/kg) via 
+    w = epsilon*e_s/(P-e_s) from AMS Glossary of Meteorology Definition'''
+    w = eps*sat_vapor_pressure(T)/(P-sat_vapor_pressure(T)) 
     return w
 def mixing_ratio_line(P, w_s):
-    e_s = (w_s*P)/(w_s+eps) #hPa and kg/kg input, hPa output
-    T = sat_vapor_temperature(e_s) #celsius output
+    '''Takes a pressure (hPa) and mixing ratio (kg/kg) and returns temperature (C) via mixing ratio definition and eqn 11 (Bolton, 1980)'''
+    e_s = (w_s*P)/(w_s+eps) 
+    T = sat_vapor_temperature(e_s) 
     return T
 def RH(T, P, w):
-    RH = 100*w/sat_mixing_ratio(P, T) #celsius and hPa input, kg/kg input, RH in %
+    '''Takes a pressure (hPa), temperature (C), and mixing ratio (kg/kg) and returns relative humidity (%) '''
+    RH = 100*w/sat_mixing_ratio(P, T) 
     return RH
 def T_LCL(T, RH):
-    A = 1/(T+ C_to_K-55) # celsius input, RH % input
+    '''Takes a temperature (C) and relative humidity (%) and returns the temperature (K) at the lifting condensation level via eqn 22 (Bolton, 1980)'''
+    A = 1/(T+ C_to_K-55)
     B = np.log(RH/100.0)/2840.0
-    T = 1/(A-B) #kelvin output
+    T = 1/(A-B)
     return T
 def theta_dry(theta, P, P_0 = 1000.):
-    T = theta*(P_0/P)**(-1*k_dry) #kelvin input, kelvin output
+    '''Takes a potential temperature (K) and returns the dry potential temperature (K) via eqn 23 (Bolton, 1980)'''
+    T = theta*(P_0/P)**(-1*k_dry) 
     return T
 def pseudoeq_potential_T(T, P, w, P_0 = 1000.0):
-    rh = RH(T, P, w) #celsius, hPa, kg/kg input and kelvin output
+    '''Takes a temperature (C), pressure (hPa), mixing ratio (kg/kg), and reference pressure (hPa) and returns the pseudoequivalent potential temperature (K) via eqn 43 (Bolton, 1980)'''
+    rh = RH(T, P, w) 
     T_lcl = T_LCL(T, rh)
     A = ((3.376/T_lcl)-0.00254)*w*1000*(1+0.81*(w))
     theta_ep = np.exp(A)*(T+C_to_K)*(P_0/P)**(0.2854*(1-0.28*(w)))
     return theta_ep
 def theta_ep_field(T, P, P_0 = 1000.0):
+    '''Takes temperature (C), pressure (hPa), and reference pressure (hPa) and returns pseudoequivalent potential temperature (K), eliminating the need to input mixing ratio'''
     w = sat_mixing_ratio(P,T)
     theta_ep = pseudoeq_potential_T(T, P, w, P_0)
     return theta_ep
